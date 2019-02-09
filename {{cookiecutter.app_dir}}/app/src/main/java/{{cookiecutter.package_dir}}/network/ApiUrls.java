653565731e9a6b898f3b342b97e9a3266298f691
Pawan <pawan.kumar@gonuclei.com>
2019-02-10 02:13:45 +0530

    Added Read Me file

--- {{cookiecutter.app_dir}}/app/src/main/java/{{cookiecutter.package_dir}}/network/ApiUrls.java
+++
@@ -1,9 +0,0 @@
-package {{cookiecutter.package_dir.replace('/','.')}}.network;
-
-public class ApiUrls {
-    public static final String FACTS_URL = "facts.json";
-
-    public static String getBaseUrl(){
-        return "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/";
-    }
-}
