b08dfefc1216405283116c2fa50459362eea968c
Pawan <pawan.kumar@gonuclei.com>
2019-02-10 11:25:51 +0530

    changes for adapter implementation

--- {{cookiecutter.app_dir}}/app/src/main/java/{{cookiecutter.package_dir}}/features/HomeFragment.java
+++ {{cookiecutter.app_dir}}/app/src/main/java/{{cookiecutter.package_dir}}/features/HomeFragment.java
@@ -1,10 +1,14 @@
 package {{cookiecutter.package_dir.replace('/','.')}}.features;
 
+import android.app.Activity;
 import android.arch.lifecycle.Observer;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +24,9 @@ import timber.log.Timber;
 
 public class HomeFragment extends BaseFragment<HomeViewModel> {
     private FragmentHomeBinding mFragmentHomeBinding;
-    public static HomeFragment newInstance(){
+    private FactsAdapter mFactAdapter;
+
+    public static HomeFragment newInstance() {
         return new HomeFragment();
     }
 
@@ -29,19 +35,65 @@ public class HomeFragment extends BaseFragment<HomeViewModel> {
         super.onCreate(savedInstanceState);
         getAppComponent().inject(this);
         initViewModel(HomeViewModel.class);
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mFragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home,container,false);
-        mViewModel.getFacts().observe(getViewLifecycleOwner(), new Observer<List<Fact>>() {
-            @Override
-            public void onChanged(@Nullable List<Fact> facts) {
-                Timber.d(facts.toString());
+        mFragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        initViews();
+        loadData();
+        mViewModel.getError().observe(getViewLifecycleOwner(), error -> {
+            if (!TextUtils.isEmpty(error)) {
+                Activity activity = getActivity();
+                Utilities.showError(activity, error);
             }
         });
+
         return mFragmentHomeBinding.getRoot();
+    }
+
+    private void loadData() {
+        if (Utilities.isOnline(getContext())) {
+            mViewModel.getFacts().observe(getViewLifecycleOwner(), facts -> {
+                loadDataToView(facts);
+                mFragmentHomeBinding.swipeContainer.setRefreshing(false);
+            });
+        } else {
+            Utilities.showError(getActivity(), getString(R.string.not_connected_to_internet));
+        }
+    }
+
+    private void initViews() {
+        mFactAdapter = new FactsAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(mFragmentHomeBinding.recyclerView.getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mFragmentHomeBinding.recyclerView.setLayoutManager(layoutManager);
+        mFragmentHomeBinding.recyclerView.setHasFixedSize(true);
+        mFragmentHomeBinding.recyclerView.setAdapter(mFactAdapter);
+        mFragmentHomeBinding.swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadData();
+            }
+        });
+    }
+
+    private void showData(boolean show) {
+        int visibility = show ? View.GONE : View.VISIBLE;
+        mFragmentHomeBinding.emptyView.setVisibility(visibility);
+    }
+
+    private void loadDataToView(List<Fact> facts) {
+        if (facts != null) {
+            if (facts.size() > 0) {
+                mFactAdapter.updateData(facts);
+                showData(true);
+            } else {
+                showData(false);
+            }
+        }
     }
 }
 
